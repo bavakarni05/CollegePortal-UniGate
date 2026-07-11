c7bf44353c2ec4df10e5e42f50520acd7201a96b
bavakarni05 <bavakarnig@gmail.com>
2026-07-11 17:10:10 +0530

    modified:   CollegePortal-main/frontend/src/config.js

--- CollegePortal-main/frontend/src/config.js
+++ CollegePortal-main/frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://collegebackend-7.onrender.com/api'.replace(/\s/g, '');
-export const BACKEND_URL = 'https://collegebackend-7.onrender.com'.replace(/\s/g, '');
+export const API_BASE_URL = 'https://collegeportal-unigate-backend.onrender.com/api'.replace(/\s/g, '');
+export const BACKEND_URL = 'https://collegeportal-unigate-backend.onrender.com'.replace(/\s/g, '');
